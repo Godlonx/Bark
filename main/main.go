17130f592c0037534fa907c86fe4709eeb15ea40
Evan Lb <evan.lebourhis@ynov.com>
2023-05-24 14:09:25 +0200

    front

--- main/main.go
+++ main/main.go
@@ -1,28 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
-	"database/sql"
-	_"github.com/mattn/go-sqlite3"
-	"net/http"
 	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var port = ":8080"
 
 type User struct {
-	Id	int
-	Pseudo string
+	Id       int
+	Pseudo   string
 	Password string
 }
 
 var user User
 
-
 func main() {
-	
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	fs := http.FileServer(http.Dir("../assets/"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	http.HandleFunc("/home", Home)
 
 	fmt.Println("(http://localhost"+port+"/home"+") - Server started on port", port)
@@ -31,27 +32,26 @@ func main() {
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	user := Sql()
-	t := template.Must(template.ParseFiles("template/home.html"))
-	t.Execute(w,user)
+	t := template.Must(template.ParseFiles("template/login.html"))
+	t.Execute(w, user)
 }
 
-func Sql() User{
+func Sql() User {
 
 	db, err := sql.Open("sqlite3", "public/barkData.db")
-	
+
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-
 	rows, err := db.Query("select * from user")
-	
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	for rows.Next() {
-		err := rows.Scan(&user.Id,&user.Pseudo,&user.Password)
+		err := rows.Scan(&user.Id, &user.Pseudo, &user.Password)
 		if err != nil {
 			log.Fatal(err)
 		}
