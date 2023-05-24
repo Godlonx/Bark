04cb782b26190e63e3fcb271b37ca0f512bec9ad
Evan Lb <evan.lebourhis@ynov.com>
2023-05-24 16:14:24 +0200

    register form

--- main/main.go
+++ main/main.go
@@ -18,9 +18,16 @@ type User struct {
 	Password string
 }
 
-type FormData struct {
+type LoginData struct {
 	Email    string
 	Password string
+}
+
+type RegisterData struct {
+	Email         string
+	Password      string
+	Username      string
+	Passwordverif string
 }
 
 var user User
@@ -28,24 +35,43 @@ var user User
 func main() {
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.HandleFunc("/home", Home)
+	http.HandleFunc("/login", Login)
+	http.HandleFunc("/register", Register)
 
-	fmt.Println("(http://localhost"+port+"/home"+") - Server started on port", port)
+	fmt.Println("(http://localhost"+port+"/login"+") - Server started on port", port)
 	http.ListenAndServe(port, nil)
 }
 
-func Home(w http.ResponseWriter, r *http.Request) {
+func Login(w http.ResponseWriter, r *http.Request) {
 	user := Sql()
 	t := template.Must(template.ParseFiles("template/login.html"))
 
 	email := r.FormValue("email")
 	password := r.FormValue("password")
-	data := FormData{}
+	data := LoginData{}
 	data.Email = email
 	data.Password = password
 	println(data.Email)
 	println(data.Password)
 	t.Execute(w, user)
+}
+
+func Register(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("template/register.html"))
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	email := r.FormValue("email")
+	passwordverif := r.FormValue("passwordverif")
+	data := RegisterData{}
+	data.Email = email
+	data.Password = password
+	data.Username = username
+	data.Passwordverif = passwordverif
+	println(data.Username)
+	println(data.Email)
+	println(data.Password)
+	println(data.Passwordverif)
+	t.Execute(w, nil)
 }
 
 func Sql() User {
