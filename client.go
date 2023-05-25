57cca1d1aacfba37b9f7a9c58c05a83cd4e5a9b5
Godlonx <godlonx44@gmail.com>
2023-05-25 13:15:11 +0200

    :construction:

--- client.go
+++ client.go
@@ -1,14 +1,37 @@
 package bark
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
 )
 
 func Client() {
 	// on écoute sur le port 8080
-	_, err := net.Dial("tcp", fmt.Sprintf(IP+":"+PORT))
+	conn, err := net.Dial("tcp", fmt.Sprintf(IP+":"+PORT))
 	if err != nil {
 		panic(err)
+	}
+	for {
+		// entrée utilisateur
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("client: ")
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+
+		// On envoie le message au serveur
+		conn.Write([]byte(text))
+
+		// On écoute tous les messages émis par le serveur et on rajouter un retour à la ligne
+		message, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+
+		// on affiche le message utilisateur
+		fmt.Print("serveur : " + message)
 	}
 }
--- server.go
+++ server.go
@@ -27,4 +27,27 @@ func Server() {
 
 	// Information sur les clients qui se connectent
 	fmt.Println("Un client est connecté depuis le port", conn.RemoteAddr())
+
+	for {
+		// On écoute les messages émis par les clients
+		buffer := make([]byte, 4096)       // taille maximum du message qui sera envoyé par le client
+		length, err := conn.Read(buffer)   // lire le message envoyé par client
+		message := string(buffer[:length]) // supprimer les bits qui servent à rien et convertir les bytes en string
+
+		if err != nil {
+			fmt.Println("Le client s'est déconnecté")
+			break
+		}
+
+		if message == "exit" {
+			break
+		}
+
+		// on affiche le message du client en le convertissant de byte à string
+
+		fmt.Print("Client:", message)
+
+		// On envoie le message au client pour qu'il l'affiche
+		conn.Write([]byte(message + "\n"))
+	}
 }
