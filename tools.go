0f87836c9b777a59832e6ca4ccd58370e83af816
smathis <mathis.silotia@ynov.com>
2023-06-02 14:15:39 +0200

    comment

--- tools.go
+++ tools.go
@@ -2,6 +2,7 @@ package bark
 
 import(
 	//"fmt"
+	//"unicode"
 )
 type registerError string
 const (
@@ -12,35 +13,35 @@ const (
 )
 
 func Check(registerData RegisterData) (bool,registerError){
-	if (!verifyPassword(registerData.Password)) {
-		return false,BadPassword
-	}
+	// if (!verifyPassword(registerData.Password)) {
+	// 	return false,BadPassword
+	// }
 	if (registerData.Password != registerData.Passwordverif) {
 		return false,UnequalPassword
 	}
 	return true,None
 }
 
-func verifyPassword(s string) (bool) {
-    letters := 0
-    for _, c := range s {
-        switch {
-        case unicode.IsNumber(c):
-            number = true
-        case unicode.IsUpper(c):
-            upper = true
-            letters++
-        case unicode.IsPunct(c) || unicode.IsSymbol(c):
-            special = true
-        case unicode.IsLetter(c) || c == ' ':
-            letters++
-        default:
-            return false
-        }
-    }
-    sevenOrMore = letters >= 7
-	if (number&&upper&&special&&sevenOrMore) {
-		return true
-	}
-    return false
-}
+// func verifyPassword(s string) (bool) {
+//     letters := 0
+//     for _, c := range s {
+//         switch {
+//         case unicode.IsNumber(c):
+//             number := true
+//         case unicode.IsUpper(c):
+//             upper := true
+//             letters++
+//         case unicode.IsPunct(c) || unicode.IsSymbol(c):
+//             special := true
+//         case unicode.IsLetter(c) || c == ' ':
+//             letters++
+//         default:
+//             return false
+//         }
+//     }
+//     sevenOrMore = letters >= 7
+// 	if (number&&upper&&special&&sevenOrMore) {
+// 		return true
+// 	}
+//     return false
+// }
