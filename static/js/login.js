3bd66dd8b83d5c87fc2de64408b907151462206e
Evan Lb <evan.lebourhis@ynov.com>
2023-06-13 13:19:36 +0200

    btn close pop up

--- static/js/login.js
+++ static/js/login.js
@@ -4,3 +4,10 @@ function openPopup() {
     var page = document.getElementsByClassName('login')[0];
     page.classList.add('blured');
 }
+
+function closePopup() {
+    var popup = document.getElementsByClassName('pop-up')[0];
+    popup.classList.remove('active');
+    var page = document.getElementsByClassName('login')[0];
+    page.classList.remove('blured');
+}
